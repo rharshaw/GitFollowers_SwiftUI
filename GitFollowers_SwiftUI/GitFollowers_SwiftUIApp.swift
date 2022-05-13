78e7887bd5ac13cd4b8681eaa8999ed4c6672e3d
Rian <63377157+rharshaw@users.noreply.github.com>
2022-05-13 11:24:16 -0400

    Initial Commit

---
+++ GitFollowers_SwiftUI/GitFollowers_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitFollowers_SwiftUIApp.swift
+//  GitFollowers_SwiftUI
+//
+//  Created by Rian Harshaw on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct GitFollowers_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
